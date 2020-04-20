560690c8029e7ccd1c33796e8067cfad51119da2
ryannathanwilson <ryan.nathan.wilson@gmail.com>
2020-04-20 13:21:33 -0400

    created fibonacci function

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
-
+const fibonacci = function(inp) {
+    let input = parseInt(inp);
+    if (input < 1) return "OOPS";
+    if (input === 1 || input === 2) return 1;
+    return fibonacci(input - 1) + fibonacci(input - 2);
 }
+console.log(fibonacci(8));
 
 module.exports = fibonacci
